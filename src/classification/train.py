66ab684a2077a0f1a7390ae012d741d07251ce59
Atreya Choudhury <atreyachoudhury@hotmail.com>
2024-06-04 21:21:53 +0200

    colbert class

--- src/classification/train.py
+++ src/classification/train.py
@@ -9,7 +9,11 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 from sklearn.metrics import classification_report, accuracy_score
-from transformer_models import SequenceClassificationDataset, evaluate_epoch, train_model
+from transformer_models import (
+    SequenceClassificationDataset,
+    evaluate_epoch,
+    train_model,
+)
 import argparse
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
 from utils import *
@@ -18,16 +22,17 @@ import yaml
 
 # def train_model(trainset, model_name):
 # def evaluate_epoch(model, dataset):
-# class SequenceClassificationDataset(Dataset), 	def __init__(self, x, y, tokenizer):
+# class SequenceClassificationDataset(Dataset), 	def __init__(self, x, y tokenizer):
 
 
 def load_yaml(yaml_file_path):
-    '''Load yaml file and return dictionary'''
-    with open(yaml_file_path, 'r') as stream:
-        try:
+    """Load yaml file and return dictionary"""
+    try:
+        with open(yaml_file_path, "r") as stream:
             return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+    except yaml.YAMLError as exc:
+        print(exc)
+
 
 def get_splits(X, y):
     skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
@@ -42,41 +47,56 @@ def get_splits(X, y):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='distilbert-base-uncased')
-    parser.add_argument('--num_epochs', type=int, default=3)
-    parser.add_argument("--gradient_accumulation_steps", type=int, default=1,
-                help="Number of updates steps to accumulate before performing a backward/update pass.")
-    parser.add_argument("--batch_size", default=64, type=int,
-                help="Batch size per GPU/CPU for training.")
-    parser.add_argument("--learning_rate", default=2e-5, type=float,
-                help="The initial learning rate for Adam.")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
-                help="Epsilon for Adam optimizer.")
-    parser.add_argument("--only_prediction", default=None, type=str,
-                help="Epsilon for Adam optimizer.")
-    parser.add_argument('--do_save', action='store_true')
+    parser.add_argument("--model_name", type=str, default="distilbert-base-uncased")
+    parser.add_argument("--num_epochs", type=int, default=3)
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=1,
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
+    )
+    parser.add_argument(
+        "--batch_size",
+        default=64,
+        type=int,
+        help="Batch size per GPU/CPU for training.",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        default=2e-5,
+        type=float,
+        help="The initial learning rate for Adam.",
+    )
+    parser.add_argument(
+        "--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer."
+    )
+    parser.add_argument(
+        "--only_prediction", default=None, type=str, help="Epsilon for Adam optimizer."
+    )
+    parser.add_argument("--do_save", action="store_true")
     parser.add_argument("--n_labels", default="10000", type=str, help="")
     args = parser.parse_args()
 
-
-    #args.save_path = "no-trainer-" + args.model_name + "-" + args.n_labels
+    # args.save_path = "no-trainer-" + args.model_name + "-" + args.n_labels
 
     # args = load_yaml("config.yaml") # args is now a dictionary loaded from yaml file
 
-    train, dev, test, passage2labelid = load_data(dataframe = "data/top_" + args.n_labels + "_training_data_NAACL.csv.gz")
+    train, dev, test, passage2labelid = load_data(
+        dataframe="data/top_" + args.n_labels + "_training_data_NAACL.csv.gz"
+    )
     if os.path.exists(args.n_labels + "_labelmap.json"):
         pass
     else:
         with open(args.n_labels + "_labelmap.json", "w") as outfile:
             json.dump(passage2labelid, outfile)
-    
+
     counter = 0
     to_save = []
     out = []
 
     args.save_path = "naacl-finetuned-" + args.model_name + "-" + str(args.n_labels)
-                
-    label2passageid = {i:j for i,j in passage2labelid.items()}
+
+    label2passageid = {i: j for i, j in passage2labelid.items()}
 
     X_train = train.destination_context.tolist()
     y_train = [passage2labelid[row.passage_id] for _, row in train.iterrows()]
@@ -84,9 +104,13 @@ if __name__ == "__main__":
     X_dev = dev.destination_context.tolist()
     y_dev = [passage2labelid[row.passage_id] for _, row in dev.iterrows()]
     try:
-        tokenizer = AutoTokenizer.from_pretrained(args.model_name, truncation_side="left")
+        tokenizer = AutoTokenizer.from_pretrained(
+            args.model_name, truncation_side="left"
+        )
     except:
-        tokenizer = AutoTokenizer.from_pretrained("roberta-base", truncation_side="left")
+        tokenizer = AutoTokenizer.from_pretrained(
+            "roberta-base", truncation_side="left"
+        )
 
     trainset = SequenceClassificationDataset(X_train, y_train, tokenizer)
     devset = SequenceClassificationDataset(X_dev, y_dev, tokenizer)
@@ -96,16 +120,15 @@ if __name__ == "__main__":
     tokenizer.save_pretrained(args.save_path)
 
     targets, outputs, probs = evaluate_epoch(model, devset, args)
-    print ("accuracy", accuracy_score(y_dev, outputs))
+    print("accuracy", accuracy_score(y_dev, outputs))
 
-
-    # sbatch --time=60 --mem-per-cpu=10000 --gpus=1 --gres=gpumem:10G --wrap="python climatebert_baseline.py" 
+    # sbatch --time=60 --mem-per-cpu=10000 --gpus=1 --gres=gpumem:10G --wrap="python climatebert_baseline.py"
     # sbatch --time=60 --mem-per-cpu=10000 --gpus=1 --gres=gpumem:10G --wrap="python climatebert_baseline.py --model_name climatebert/distilroberta-base-climate-f"
     # sbatch --time=60 --mem-per-cpu=10000 --gpus=1 --gres=gpumem:10G --wrap="python climatebert_baseline.py --model_name roberta-base"
     # sbatch --time=60 --mem-per-cpu=10000 --gpus=1 --gres=gpumem:10G --wrap="python climatebert_baseline.py --model_name ../refactored-environmental-claims/src/envclaim-climatebert"
 
     # sbatch --time=1440 --mem-per-cpu=24000 --gpus=1 --gres=gpumem:10G --wrap="python src-classification/training_without_trainer.py --n_labels 50000"
-    
+
     # sbatch --time=240 --mem-per-cpu=12000 --gpus=1 --gres=gpumem:10G --wrap="python src-classification/training_without_trainer.py --n_labels 10000 --model_name nlpaueb/legal-bert-base-uncased"
     """
 sbatch --time=240 --mem-per-cpu=12000 --gpus=1 --gres=gpumem:10G --wrap="python src-classification/training_without_trainer.py --n_labels 10000"
@@ -120,4 +143,3 @@ sbatch --time=2880 --mem-per-cpu=12000 --gpus=1 --gres=gpumem:10G --wrap="python
 sbatch --time=1440 --mem-per-cpu=12000 --gpus=1 --gres=gpumem:10G --wrap="python src-classification/training_without_trainer.py --n_labels 10000 --model_name roberta-large --batch_size 16"
 
     """
- 
---
+++ src/colbert.py
@@ -0,0 +1,94 @@
+import yaml
+import numpy as np
+import torch
+import platform
+import json
+from csv import DictReader
+from ragatouille import RAGPretrainedModel, RAGTrainer
+from sklearn.model_selection import StratifiedKFold
+
+
+def load_yaml(yaml_file_path):
+    """Load yaml file and return dictionary"""
+    try:
+        with open(yaml_file_path, "r") as stream:
+            return yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
+def get_splits(X, y):
+    skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+    X = np.array(X)
+    y = np.array(y)
+    skf.get_n_splits(X, y)
+    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+        X_train, y_train = X[train_index], y[train_index]
+        X_test, y_test = X[test_index], y[test_index]
+        yield X_train, y_train, X_test, y_test
+
+
+class ColBERT:
+    """train and index ColBERT on legal training data and use it for search"""
+
+    def __init__(self) -> None:
+        pass
+
+    def load_passages(self, path="./data/passage_dict.json") -> None:
+        """load dictionary of passage_ids and passages"""
+
+        with open(path, "r") as f:
+            self.passages = json.load(f)
+        self.passages = self.passages["data"]
+
+    def load_train_data(self, path="./data/top_10000.csv") -> None:
+        with open(path, mode="r", newline="", encoding="utf-8") as f:
+            self.train_reader = DictReader(f)
+
+    def setup_model(self, train=False) -> None:
+        if not train:
+            self.model = RAGPretrainedModel.from_pretrained(("colbert-ir/colbertv2.0"))
+            pass
+        trainer = RAGTrainer(
+            model_name="Test_ColBERT",
+            pretrained_model_name="colbert-ir/colbertv2.0",
+            language_code="en",
+        )
+        trainer.prepare_training_data(
+            raw_data=train,
+            data_out_path="./data/",
+            all_documents=documents,
+            num_new_negatives=0,
+            mine_hard_negatives=False,
+        )
+        trainer.train(
+            batch_size=32,
+            nbits=4,  # How many bits will the trained model use when compressing indexes
+            maxsteps=50,  # Maximum steps hard stop
+            use_ib_negatives=True,  # Use in-batch negative to calculate loss
+            dim=128,  # How many dimensions per embedding. 128 is the default and works well.
+            learning_rate=5e-6,  # Learning rate, small values ([3e-6,3e-5] work best if the base model is BERT-like, 5e-6 is often the sweet spot)
+            doc_maxlen=64,  # Maximum document length. Because of how ColBERT works, smaller chunks (128-256) work very well.
+            use_relu=False,  # Disable ReLU -- doesn't improve performance
+            warmup_steps="auto",  # Defaults to 10%
+        )
+
+    def index(self) -> None:
+        self.model.index(
+            collection=self.passages.values(),
+            document_ids=self.passages.keys,
+            # document_metadatas=document_metadata,
+            index_name="LePaRD_pretrained",
+            max_document_length=64,
+            split_documents=False,
+        )
+
+    def device(self) -> str:
+        if platform.system() == "Darwin":
+            if torch.backends.mps.is_available():
+                self.device = torch.device("mps")
+                return "mps"
+        elif platform.system() == "Windows":
+            if torch.cuda.is_available():
+                self.device = torch.device("cuda")
+                return "cuda"
